a5040cf8144e1e83e9808ba64165ac9d1f8efb6d
obed <obed@obed-virtual-machine>
2019-06-04 17:45:51 +0530

    updated...

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -4,7 +4,7 @@ public class WelcomeMessage {
 	public static void main(String[] args) {
 		// To display the welcome message
 
-		System.out.println("Welcome to java programming !!!.");
+		System.out.println("Welcome to Saveetha Engineering College !!!.");
 	}
 
 }
