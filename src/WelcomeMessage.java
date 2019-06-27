c3869772e99b616238ee8e1d7b472a8a7c6b3c8a
user2 <user2@administrator-HP-Pro-3330-MT>
2019-06-27 10:49:33 +0530

    printing five times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,10 +1,15 @@
-
+/****
+ * welcome mwessage
+ * @author user2lakshmi
+ *
+ */
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
-		// To display the welcome message
-
+		{for(int i=0;i<=5;i++)
+		// To display the welcome messa
 		System.out.println("Welcome to java programming !!!.");
 	}
 
+}
 }
