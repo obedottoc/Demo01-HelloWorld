59867052cf1f729978ecd0c25d4c1628edbbdc6b
user2 <user2@simlab23>
2019-06-27 21:47:54 +0530

    printing five times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,10 +1,17 @@
-
+/*****
+ * this program is to print welcome message five times
+ * @author lokesh
+ *
+ */
 public class WelcomeMessage {
 
-	public static void main(String[] args) {
-		// To display the welcome message
+	public static void main(String[] args)
+	{
+		for (int i=0;i<5;i++)
+		{
+			// To display the welcome message
 
 		System.out.println("Welcome to java programming !!!.");
 	}
 
-}
+}}
