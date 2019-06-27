cd37ed93affd1fa5ce6350b8dcfc3b0b2ac03722
user2 <user2@simlab20>
2019-06-27 21:59:07 +0530

    printing five times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,10 +1,14 @@
-
+/****
+ * welcome messsge
+ * @author user2
+ *
+ */
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
+		{for(int i=0;i<=5;i++)
 		// To display the welcome message
-
 		System.out.println("Welcome to java programming !!!.");
 	}
-
+}
 }
