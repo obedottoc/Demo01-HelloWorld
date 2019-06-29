b2685502d7a6e53200451a9257ee0a9ef985cb54
user2 <user2@administrator-HP-Pro-3330-MT>
2019-06-29 09:14:37 +0530

    change is to print 50 times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,9 +1,13 @@
-
+/*
+ *this program is to print welcome message 50times
+ *it is developed by sk
+ *@uthor suriya kumar
+ */
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		// To display the welcome message
-
+			for(int i=0;i<=50;i++)
 		System.out.println("Welcome to java programming !!!.");
 	}
 
