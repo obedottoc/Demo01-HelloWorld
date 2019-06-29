d424ef76c4845cc7d89dbeb7e3d3ee8cb54abf62
user2 <user2@simlab20>
2019-06-29 20:15:59 +0530

    The program gives welcome address for 50 times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,9 +1,13 @@
-
+/********
+ * this program is to print welcome message
+ * @author user2 Rositha
+ *
+ */
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		// To display the welcome message
-
+for (int i = 1;i <=10; i++)
 		System.out.println("Welcome to java programming !!!.");
 	}
 
