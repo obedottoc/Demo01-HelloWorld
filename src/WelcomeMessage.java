e9ac01c50ad5eff922a9e21913ef34303e3b3c79
user2 <user2@simlab23>
2019-06-29 20:08:54 +0530

    for printing 50 times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,9 +1,16 @@
-
+/*****
+ * welcome program to print 50 times
+ * @author jacob
+ *
+ */
+ 
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		// To display the welcome message
-
+		
+			for (int i=0;i<=50;i++)
+		
 		System.out.println("Welcome to java programming !!!.");
 	}
 
