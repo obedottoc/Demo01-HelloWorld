703f3e3b98f653269e186debdc536474efa98a1c
user2 <user2@administrator-HP-Pro-3330-MT>
2019-06-29 09:08:14 +0530

    This programme is use to produce the text 50 times.

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,9 +1,14 @@
+/***
+ * This program is to print WelcomeMessage 50 times
+ * @author monicar
+ *
+ */
 
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		// To display the welcome message
-
+for(int i=0; i<=50; i++)
 		System.out.println("Welcome to java programming !!!.");
 	}
 
