6810d3b11cd6bdca0fdd7a873ce7ee76f44458b0
user2 <user2@administrator-HP-Pro-3330-MT>
2019-06-29 08:40:45 +0530

    to ptint 50 times

--- src/WelcomeMessage.java
+++ src/WelcomeMessage.java
@@ -1,9 +1,14 @@
 
+/*****
+ * this program is to print welcome message 50 times
+ * @author pavithra
+ *
+ */
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		// To display the welcome message
-
+     for(int i=1;i<=50;i++)
 		System.out.println("Welcome to java programming !!!.");
 	}
 
